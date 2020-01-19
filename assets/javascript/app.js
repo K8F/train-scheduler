2d59ad353df9830cf0014f283af91b68d340d04a
Kate Foust <kate.e.foust@gmail.com>
2020-01-19 12:10:57 -0800

    app is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,24 +22,37 @@ $(document).ready(function () {
     database.ref().on("child_added", function (snap) {
         console.log(snap.key, snap.val())
         var train = snap.val()
-
+        var info = calculateNext(train.firstTrain, train.frequency)   // we can return an array with the info (nexttrain, minutesaway)
         var rowTrain = `
             <tr>
                 <td>${train.trainName}</td>
                 <td>${train.destination}</td>
                 <td>${train.frequency}</td>  
+                <td>${info[0]}</td>
+                <td>${info[1]}</td>
             </tr>`
         $("#add-row").append(rowTrain);
 
     });  
 
-
-
-
 //calculate train times
     //calculate the difference between the first train and frequency (min)
     //caclulate remainder of diff & frequency
     //subtract frequency from the remainder
+    function calculateNext(first, freq){
+        var firstTrain = moment(first, "HH:mm:ss")
+        console.log("first:", firstTrain)
+        var diff = moment().diff(firstTrain, "minutes")   //current*60  - first*60 = minutes
+        var tRemainder = diff % freq
+        var minutesAway = freq - tRemainder    //08:00  minutes in abosulutes   //substract (diff)
+        var nextTrain = moment().add(minutesAway, "minutes").format("hh:mm A")   // plus (sum)
+         //moment()= current time
+         
+         console.log(nextTrain , minutesAway)
+         return([nextTrain, minutesAway])
+     
+     }
+ 
 
 
 //add new trains
@@ -67,13 +80,6 @@ $(document).ready(function () {
 
 
     )
-
-
-
-
-
-
-
 
 
 });
